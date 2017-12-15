0cb3fa6ef2c09685b162039e858323d705c692b6
Leon <Leon.geim@googlemail.com>
2017-12-15 09:59:44 +0100

    Anpassungen Aufgabe 2

--- Arrays/Aufgabe2.c
+++ Arrays/Aufgabe2.c
@@ -1,20 +1,24 @@
-//#include <stdio.h>
-//
-//void main()
-//{
-//	char satz[124] = "Der maximale Ertrag der subterraren Agrarprodukte steht im reziproken Verhältnis zur geistigen Kapazität ihrer Produzenten";
-//	
-//	for (int i = 0; i < 26; i++)
-//	{
-//		for (int j = 0; j < 126; j++)
-//		{
-//			if ((int)satz[j] == i + (int)'A' || (int)satz[j] == i + (int)'a' ||
-//				(int)satz[j] == i + (int)'Ä' || (int)satz[j] == i + (int)'ä')
-//			{
-//				printf("%c", satz[j]);
-//			}
-//		}
-//				
-//	}
-//	getchar();
-//}
+#include <stdio.h>
+
+void main()
+{
+	char satz[] = "Der maximale Ertrag der subterraren Agrarprodukte steht im reziproken Verhältnis zur geistigen Kapazität ihrer Produzenten";
+	
+	for (int i = 0; i < 26; i++)
+	{
+		for (int j = 0; j < sizeof(satz) / sizeof(char); j++)
+		{
+			if ((int)satz[j] == i + (int)'A' || (int)satz[j] == i + (int)'a')				
+			{
+				printf("%c", satz[j]);
+			}
+
+			if (i < 3 && ((int)satz[j] == i + (int)'Ä' || (int)satz[j] == i + (int)'ä'))
+			{
+				printf("%c", satz[j]);
+			}
+		}
+				
+	}
+	getchar();
+}
--- Arrays/Aufgabe5.c
+++ Arrays/Aufgabe5.c
@@ -1,30 +1,30 @@
-#include <stdio.h>
-
-void main()
-{
-	int zahlen1[13];
-	int zahlen2[13];
-	int array3[13];
-
-	for (int i = 0; i < 11; i++)
-	{
-		zahlen1[i] = i;
-	}
-
-	for (int i = 10; i < 23; i++)
-	{
-		zahlen2[i-10] = i;
-	}
-
-	for (int i = 0; i < 13; i++)
-	{
-		array3[i] = zahlen1[i] + zahlen2[i];
-	}
-
-	for (int i = 0; i < 13; i++)
-	{
-		printf("%i\t%i\t%i\t\n", zahlen1[i], zahlen2[i], array3[i]);
-	}
-
-	getchar();
-}
+//#include <stdio.h>
+//
+//void main()
+//{
+//	int zahlen1[13];
+//	int zahlen2[13];
+//	int array3[13];
+//
+//	for (int i = 0; i < 11; i++)
+//	{
+//		zahlen1[i] = i;
+//	}
+//
+//	for (int i = 10; i < 23; i++)
+//	{
+//		zahlen2[i-10] = i;
+//	}
+//
+//	for (int i = 0; i < 13; i++)
+//	{
+//		array3[i] = zahlen1[i] + zahlen2[i];
+//	}
+//
+//	for (int i = 0; i < 13; i++)
+//	{
+//		printf("%i\t%i\t%i\t\n", zahlen1[i], zahlen2[i], array3[i]);
+//	}
+//
+//	getchar();
+//}
